5c286ed0ba5f66438ba04dce1cd030f90f6a36b5
“git_name” <git@qq.com>
2021-02-20 11:09:13 +0800

    remove

--- ADHD/run_chrome.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("pytest --browser=chrome ./scripts/test01_admin_login.py --alluredir ./report/allure-results")
-# os.system("pytest --browser=chrome --alluredir ./report/allure-results")
-os.system("allure generate ./report/allure-results -o ./report/allure-html --clean")
