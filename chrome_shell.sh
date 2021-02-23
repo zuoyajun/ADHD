8ce6a4ee61237bf9fda8173a975f153f699d960c
“git_name” <git@qq.com>
2021-02-23 14:05:34 +0800

    add

--- chrome_shell.sh
+++ chrome_shell.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 time=$(date "+%Y-%m-%d")
 rm -rf /usr/local/tomcat/webapps/report/chrome$time
-mv /root/.jenkins/workspace/ADHD-selenium-web-chrome/report/chrome /usr/local/tomcat/webapps/report/chrome$time
+mv /root/.jenkins/workspace/ADHD-selenium-web-Chrome/report/chrome /usr/local/tomcat/webapps/report/chrome$time
