2cec580d1899656d3357291c88c05d6997eb94f2
“git_name” <git@qq.com>
2021-02-25 15:07:48 +0800

    add

--- run_chrome.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("pytest --browser=chrome ./scripts/test01_admin_login.py --alluredir ./report/chrome/allure-results")
-# os.system("pytest --browser=chrome --alluredir ./report/chrome/allure-results")
-os.system("allure generate ./report/chrome/allure-results -o ./report/chrome/allure-html --clean")
--- run_firefox.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("pytest --browser=firefox ./scripts/test01_admin_login.py --alluredir ./report/firefox/allure-results")
-# os.system("pytest --browser=firefox --alluredir ./report/firefox/allure-results")
-os.system("allure generate ./report/firefox/allure-results -o ./report/firefox/allure-html --clean")
